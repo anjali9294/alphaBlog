a3ee561d90fa67e09a16adc050beac193d21a1d7
anjali parihar <anjaliparihar9294@gmail.com>
2021-05-19 21:41:10 +0530

    create users sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_064716) do
+ActiveRecord::Schema.define(version: 2021_05_19_095314) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "timestamps"
+    t.string "password_digest"
   end
 
 end
